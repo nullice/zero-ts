71e73ad09121c4b1f69ee8d4a94f567c955af391
nullice <ui@Nullice.com>
2019-01-21 12:31:30 +0800

    0

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+
+import shuffle from "lodash/shuffle"
+
+let ss = {
+    inx: 123,
+    inc(ss: string) {
+        shuffle([1, 2, 3, 4])
+
+        return ss.includes("1")
+    },
+
+
+    // async asyncFuc()
+    // {
+    //     return 123
+    // }
+}
+export { ss }
+
